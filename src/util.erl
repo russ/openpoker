22dfa6a1626e478eef8b30f21f67b880eface7aa
Joel Reymont <joelr@mothership.local>
2008-07-03 20:38:38 +0100

    init_db_slave

--- src/util.erl
+++ src/util.erl
@@ -2,11 +2,28 @@
 
 -module(util).
 
--export([is_process_alive/1]).
+-export([is_process_alive/1,
+         init_db_slave/1
+         ]).
 
 is_process_alive(Pid) 
   when is_pid(Pid) ->
     rpc:call(node(Pid), erlang, is_process_alive, [Pid]).
+    
+init_db_slave(MasterNode) ->
+    mnesia:start(),
+    mnesia:change_config(extra_db_nodes, [MasterNode]),
+    mnesia:change_table_copy_type(schema, node(), disc_copies),
+    Tabs = mnesia:system_info(tables) -- [schema],
+    [mnesia:add_table_copy(Tab,node(), disc_copies) || Tab <- Tabs].
 
     
-    
+
+
+
+
+
+
+
+
+              
