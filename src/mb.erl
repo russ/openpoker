d7ca7ed097407655ef864e21417d503d3a57ad8d
Joel Reymont <joelr@mothership.local>
2008-10-09 22:27:14 +0100

    don't track game prep time

--- src/mb.erl
+++ src/mb.erl
@@ -103,7 +103,6 @@ handle_cast({'RUN', Game, Barrier, Delay, Trace}, Data)
     stats:sum(games_launched, 1),
     stats:max(max_game_launch_time, timer:now_diff(T5, T1)),
     stats:avg(game_launch_time, timer:now_diff(T5, T1)),
-    stats:avg(game_prep_time, timer:now_diff(T2, T1)),
     stats:avg(game_start_time, timer:now_diff(T3, T2)),
     stats:avg(player_connect_time, timer:now_diff(T5, T4)),
     {noreply, Data1};
