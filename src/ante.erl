713c6acdb976dc5f24eff7fb3a415efd1aca03b0
Joel Reymont <joelr1@gmail.com>
2008-11-03 18:36:51 +0000

    ante

---
+++ src/ante.erl
@@ -0,0 +1,32 @@
+%%%% Copyright (C) 2005-2008 Wager Labs, SA
+
+-module(ante).
+
+-export([start/3]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("common.hrl").
+-include("texas.hrl").
+-include("pp.hrl").
+-include("game.hrl").
+
+start(Game, Ctx, []) ->
+    Game1 = g:reset_player_state(Game, ?PS_ANY, ?PS_PLAY),
+    Players = g:get_seats(Game1, ?PS_ACTIVE),
+    post_ante(Game1, Game1#game.ante, Players),
+    {stop, Game, Ctx}.
+
+post_ante(Game, _, []) ->
+    Game;
+
+post_ante(Game, Ante, [H|T]) ->
+    Seat = g:get_seat(Game, H),
+    Game1 = g:add_bet(Game, H, Ante),
+    R = #notify_call{ 
+      game = Game1#game.gid, 
+      player = Seat#seat.pid,
+      amount = Ante
+     },
+    Game2 = g:broadcast(Game1, R),
+    post_ante(Game2, Ante, T).
